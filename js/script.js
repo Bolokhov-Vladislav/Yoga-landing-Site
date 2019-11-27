a32f99d4127290944f83df62abadc8e3ffda9b89
Bolokhov Vladyslav <40151122+Bolokhov-Vladislav@users.noreply.github.com>
2019-11-27 14:25:00 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -205,4 +205,54 @@ window.addEventListener('DOMContentLoaded', function() {
                     .catch(() => statusMessage.innerHTML = message.failure)
                     .then(clearInput);
     });
+
+
+
+    // SLIDER
+    let slideIndex = 1,
+
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    function showSlides(n) {
+
+        if(n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n){
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);//1 шаг назад по слайдеру 
+    });
+    next.addEventListener('click', function() {
+        plusSlides(1);//1 шаг вперед 
+    });
+
+    dotsWrap.addEventListener('click', function(e) {
+        for (let i = 0; i < dots.length + 1; i++){
+            if(e.target.classList.contains('dot') && e.target == dots[i-1]){
+                currentSlide(i);
+            }
+        }
+    });
 });
